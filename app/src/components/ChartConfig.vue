db5a596c0c694bfe3284241eabb67837b13b8bc9
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2025-03-18 14:01:58 -0400

    fgrdxf

--- app/src/components/ChartConfig.vue
+++ app/src/components/ChartConfig.vue
@@ -1,23 +1,42 @@
-<template>
-    <Bar id="sample-chart" :options="ChartOptions" :data="ChartData"></Bar>
-</template>
-
 <script setup>
-import { Bar } from 'vue-chartjs';
-import { Chart as ChartJS, Title, Tooltip, legend, BarElement, CategorySchale, LinearScale } from 'chart.js';
-import ChartData from './ChartData.vue';
-  ChartJS.register(Title, Tooltip, legend, BarElement, CategorySchale, LinearScale);
+// const url = "https://data.cityofnewyork.us/resource/uip8-fykc.json";
 
 
+// async function getData(url) {
+//   try {
+//     const response = await fetch(url);
+//     const result = await response.json();
+//     console.log(result);
+//     return result;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
-    data() {
-    return {
-        labels: [January, February, March],
-        datasets: [ { data: [15, 40, 12]}]
+// const arrests = await getData(url);
+
+//     // id: item.arrest_key,
+//     // offense: item.ofns_desc,
+//     // ycoord: item.longitude,
+//     // xcoord: item.latitude,
+//     // race: item.perp_race,
+
+    export const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 39, 10, 40, 39, 80, 40]
     }
+  ]
+}
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false
 }
 </script>
 
-<style lang="scss" scoped>
 
-</style>
+
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import ChartData from '@/components/ChartData.vue';
+
 </script>
 
 <template>
   <main>
-    <ChartData/>
+  <ChartConfig/>
   </main>
 </template>
