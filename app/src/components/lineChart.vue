6325ecf59cf1bb5f4787702b8ce22844ffe10f44
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2025-03-21 11:02:12 -0400

    rfgtder

--- app/src/components/lineChart.vue
+++ app/src/components/lineChart.vue
@@ -1,8 +1,9 @@
 <template>
   <Line :data="data" />
+
 </template>
 
-<script lang="ts">
+<script>
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -14,7 +15,6 @@ import {
   Legend
 } from 'chart.js'
 import { Line } from 'vue-chartjs'
-import * as chartConfig from './chartConfig'
 
 ChartJS.register(
   CategoryScale,
@@ -31,8 +31,27 @@ export default {
   components: {
     Line
   },
-  data() {
-    return chartConfig
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const {WaterData} = await fetch('https://data.cityofnewyork.us/resource/ia2d-e54m.json');
+      this.chartdata = WaterData
+      console.log(WaterData);
+      
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
   }
+}
+
+const waterData = {
+  const pop = waterData("new_york_city_population");
+  const consumption
 }
 </script>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
+import lineChart from '@/components/lineChart.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/./components',
+      name: 'lineChart',
+      component: 'Line'
+    }
   ],
 })
 
